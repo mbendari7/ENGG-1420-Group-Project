0b5854675fbdbb7523e94dba9181423add3eebc4
khalidscodes <kalsabaw@uoguelph.ca>
2026-03-02 03:56:25 -0500

    added basic GUI (popup prompting user for name) for user input form

---
+++ UserInputForm.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+
+
+public class UserInputForm {
+
+    public static void main(String[] args){
+
+        String name = JOptionPane.showInputDialog("User name: ");
+
+    }
+}
