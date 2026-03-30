324abc48c9ec8ab050201539fb263877f76177fe
mostafard38 <mradwan@uoguelph.ca>
2026-03-30 00:57:18 -0400

    Change fields to public in SystemState class
    Updated SystemState so restored users, events, bookings, and booking ID data can be accessed more directly from Main - Just don't wan't any compiling errors

--- SystemState.java
+++ SystemState.java
@@ -1,11 +1,11 @@
 import java.util.ArrayList;
 
 public class SystemState {
-    ArrayList<User> allUsers;
-    ArrayList<Event> allEvents;
-    ArrayList<Booking> allBookings;
-    int nextBookingId;
-
+    public ArrayList<User> allUsers;
+    public ArrayList<Event> allEvents;
+    public ArrayList<Booking> allBookings;
+    public int nextBookingId;
+    
     public SystemState(ArrayList<User> allUsers, ArrayList<Event> allEvents, ArrayList<Booking> allBookings, int nextBookingId) {
         this.allUsers = allUsers;
         this.allEvents = allEvents;
