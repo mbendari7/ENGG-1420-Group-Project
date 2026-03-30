67e2530400f7423d6c5d8f5e6f806c6d27cd9200
mostafard38 <mradwan@uoguelph.ca>
2026-03-29 22:46:13 -0400

    Connecting Main to restored system state
    Updated Main so the program restores saved users, events, bookings, and the next booking ID at startup

--- Main.java
+++ Main.java
@@ -1,12 +1,22 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class Main {
-    // Centralized data manager for the entire application to hold system state
     public static BookingManager bookingManager = new BookingManager();
 
+    public static ArrayList<User> allUsers = new ArrayList<User>();
+
     public static void main(String[] args) {
-        // Main Navigation Menu
+
+        DataLoader loader = new DataLoader();
+        SystemState loadedState = loader.loadSystemState();
+
+        allUsers = loadedState.allUsers;
+        bookingManager.allEvents = loadedState.allEvents;
+        bookingManager.allBookings = loadedState.allBookings;
+        bookingManager.nextBookingId = loadedState.nextBookingId;
+
         JFrame mainFrame = new JFrame("Campus Event Booking System - Phase 1");
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         mainFrame.setSize(400, 350);
@@ -16,22 +26,18 @@ public class Main {
         titleLabel.setFont(new Font("Arial", Font.BOLD, 18));
         mainFrame.add(titleLabel);
 
-        // 1. User Management
         JButton userMenuBtn = new JButton("1. User Management");
         userMenuBtn.addActionListener(e -> new UserInputForm());
         mainFrame.add(userMenuBtn);
 
-        // 2. Event Management
         JButton eventMenuBtn = new JButton("2. Event Management");
         eventMenuBtn.addActionListener(e -> new EventInputForm(bookingManager));
         mainFrame.add(eventMenuBtn);
 
-        // 3. Booking Management
         JButton bookingMenuBtn = new JButton("3. Booking Management");
         bookingMenuBtn.addActionListener(e -> new BookingForm()); // Links to your BookingForm!
         mainFrame.add(bookingMenuBtn);
 
-        // 4. Waitlist Management
         JButton waitlistMenuBtn = new JButton("4. Waitlist Management");
         waitlistMenuBtn.addActionListener(e -> new WaitlistForm()); // Links to the new WaitlistForm!
         mainFrame.add(waitlistMenuBtn);
